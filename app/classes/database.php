28106434c6cb68449c2fbf751ac9b8c9e84014cc
Josh Schneider <josh@fgccfl.net>
2021-02-14 17:19:59 -0500

    Created Database class

---
+++ app/classes/database.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database
+{
+    private static $instance;
+    private static $dbPass;
+    private $connection;
+
+    private function __construct()
+    {
+        if (self::$dbPass == null) {
+            self::$dbPass = trim(file_get_contents(APP . '/.private/db'));
+        }
+        $this->connection = new PDO('mysql:host=db.fgccfl.net;dbname=extempprep4',
+            'extprep2021', self::$dbPass);
+    }
+
+    private function __clone()
+    {}
+
+    public static function connect(): Database
+    {
+        return self::$instance = self::$instance ?? new Database();
+    }
+
+    public function open(): PDO
+    {
+        return $this->connection;
+    }
+
+}
